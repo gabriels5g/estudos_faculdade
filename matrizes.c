d719a6fc0c78211aa10230bd0d489e1ba81a7eae
Gabriel S <gabrielspxls@gmail.com>
2022-10-12 19:29:39 -0300

    Add files via upload

---
+++ matrizes.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+//matrizes
+int main(){
+    float notas[3][2];
+
+    printf("Digite uma nota: ");
+
+    scanf("%f", notas[1][0]);
+
+    printf("Nota digitada: %.2f", notas[1][0]);
+}
+
+
+
+
+//#include <stdio.h>
+//int main(){
+//    int num[2] = {10, 20};
+//    printf("\n Utilizando índice do vetor: %d", num[0]);
+//    printf("\n Utilizando ponteiro: %d", *num);
+//    return 0;
+//}
+
